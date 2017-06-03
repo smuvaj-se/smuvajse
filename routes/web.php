bc757a8f88e7e345f783916bbfa8b93945292601
Sasha Miljkovic <imalimalomrk@gmail.com>
2017-06-03 20:25:36 +0200

    initial admin panel setup

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Temporary Web Routes
-|--------------------------------------------------------------------------
-*/
+// All admin routs
+require(base_path() . '/routes/admin.php');
 
 Route::get('/', function () {
     return view('welcome');
